480d2f5dea6c518926e6d3626b4a7bfc049c40aa
Alcala <al364337@uji.es>
2021-10-06 12:58:27 +0200

    Ejemplo

--- Carrito/cesta.js
+++ Carrito/cesta.js
@@ -1,4 +1,18 @@
-cesta = [];
+const { RSA_X931_PADDING } = require("constants");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log("Bienvenido\nDisponemos de estos 3 articulos\n1-Palos\n2-hierros\n3-muelles\n");
+  
+rl.question("¿Cual quieres introducir a la cesta?(introduce el nombre)\n", function (answer) {
+console.log(`Tu cesta es ${answer}`);
+rl.close();
+});
+
 
 function addProducto(evento) {
 
