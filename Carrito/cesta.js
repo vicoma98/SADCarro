e6dba41a4a87d0a7e832804d8ba921f77ab9a6ed
vicoma98 <victor.corral.martinez@gmail.com>
2021-10-08 16:20:17 +0200

    interfaz sin quitar

--- Carrito/cesta.js
+++ Carrito/cesta.js
@@ -5,17 +5,28 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+cesta = [];
+
 console.log("Bienvenido\nDisponemos de estos 3 articulos\n1-Palos\n2-hierros\n3-muelles\n");
-  
-rl.question("¿Cual quieres introducir a la cesta?(introduce el nombre)\n", function (answer) {
-console.log(`Tu cesta es ${answer}`);
-rl.close();
+
+rl.question("¿Cual quieres introducir a la cesta?(introduce el nombre)\n", (answer) => {
+
+    addProducto(answer);
+    rl.setPrompt("¿Cual quieres introducir a la cesta?(introduce el nombre)\n");
+    rl.prompt();
+});
+rl.on('line', (input) => {
+    if (input.trim() === 'salir' || input.trim() == 'exit') {
+        tostring();
+        process.exit();
+    }
+    addProducto(input);
+    rl.setPrompt("¿Cual quieres introducir a la cesta?(introduce el nombre)\n");
+    rl.prompt();
 });
 
-
 function addProducto(evento) {
-
-
+    cesta.push(evento);
 }
 
 function remevoProducto(evento) {
@@ -23,5 +34,6 @@ function remevoProducto(evento) {
 }
 
 function tostring() {
-
+    //console.log(`Tu cesta es ${answer}`);
+    console.log(cesta);
 }
