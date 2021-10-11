acad76b923e8cd5b5f7f0ab4ce536d974b91eaee
vicoma98 <victor.corral.martinez@gmail.com>
2021-10-11 11:51:22 +0200

    interfaz acabada missing mogoDB

--- Carrito/cesta.js
+++ Carrito/cesta.js
@@ -1,9 +1,7 @@
 class Carrito {
-
-    constructor(carrito) {
+    constructor() {
         this.carrito = [];
     }
-
 
     newcarrito() {
         this.carrito = [];
@@ -16,29 +14,16 @@ class Carrito {
         this.carrito.push(evento);
     }
 
-    remevoProducto(evento) {
-        sustito = [];
-        veces = 0;
-        for (const id in this.carrito) {
-            if (id === evento && veces == 0) {
-                veces += 1;
-
-            } else {
-                sustito.push(id);
-            }
-        }
-        if (this.carrito.lenght() == sustito.lenght()) {
-            return false;
-        } else {
-            this.carrito = sustito;
-            return true;
-        }
+    removeProducto(evento) {
+        const idex = this.carrito.indexOf(evento);
+        this.carrito.splice(idex, 1);
     }
 
     tostring() {
         //console.log(`Tu cesta es ${answer}`);
-        //console.log(cesta);
+        console.log(this.carrito);
         return this.carrito;
     }
 }
-//export { Carrito };
+
+module.exports.Carrito = Carrito;
--- Carrito/main.js
+++ Carrito/main.js
@@ -1,33 +1,79 @@
-const cesta = require('./cesta.js');
+let modulo = require('./cesta.js');
 const readline = require("readline");
+const inquirer = require('inquirer');
+const { async } = require('rxjs');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
 
-function main() {
+const main = async() => {
+    car = new modulo.Carrito();
+    let option = '';
 
-    car = new cesta.Carrito();
+    do {
+        option = await inqmenu();
 
-    console.log("Bienvenido\nDisponemos de estos 3 articulos\n1-Palos\n2-hierros\n3-muelles\n");
-
-    rl.question("¿Cual quieres introducir a la cesta?(introduce el nombre)\n", (answer) => {
-        car.addProducto(answer);
-        //coso.Carrito.addProducto(answer);
-        rl.setPrompt("¿Cual quieres introducir a la cesta?(introduce el nombre)\n");
-        rl.prompt();
-    });
-    rl.on('line', (input) => {
-        if (input.trim() == 'salir' || input.trim() == 'exit') {
-            tostring();
-            process.exit();
+        switch (option) {
+            case '2':
+                const pr = await leerinput('Producto: ');
+                car.addProducto(pr);
+                break;
+            case '1':
+                const eliminado = await leerinput('Producto: ');
+                car.removeProducto(eliminado);
+                break;
+            case '3':
+                car.tostring();
+                break;
         }
-        //coso.Carrito.addProducto(input);
-        rl.setPrompt("¿Cual quieres introducir a la cesta?(introduce el nombre)\n");
-        rl.prompt();
-    });
+
+
+        await pause();
+    } while (option !== '0');
+
 
 
 }
+const menuOp = [{
+    type: 'list',
+    name: 'option',
+    message: '¿que desea hacer?',
+    choices: [
+        { value: '0', name: "0. salir" },
+        { value: '1', name: "1. eliminar" },
+        { value: '2', name: "2. anadir" },
+        { value: '3', name: "3. mostrar" }
+    ]
+}];
+const inqmenu = async() => {
+    console.clear();
+
+    const { option } = await inquirer.prompt(menuOp);
+    return option;
+}
+const pause = async() => {
+    const question = [{
+        type: 'input',
+        name: 'enter',
+        message: ' Presione enter para continuar'
+    }];
+    console.log('\n');
+    await inquirer.prompt(question);
+}
+
+const leerinput = async(message) => {
+    const question = [{
+        type: 'input',
+        name: 'desc',
+        message,
+        validate(value) {
+            if (value.length === 0) {
+                return 'Ingrese algun valor';
+            }
+            return true;
+        }
+    }];
+    const { desc } = await inquirer.prompt(question);
+    return desc;
+
+}
+
 main();
